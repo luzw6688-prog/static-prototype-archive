924c3c6517b2fc6762215122f2427bbe6751aabb
mansfield <luzw6688@gmail.com>
2026-07-20 16:38:41 +0800

    config: set dashboard business timezone

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 window.APP_CONFIG = {
   supabaseUrl: 'https://reuzomfznynrzhwtmblv.supabase.co',
-  supabasePublishableKey: 'sb_publishable_q5EtMfCMg3aIhdNeDUKbig_4d_8YtAg'
+  supabasePublishableKey: 'sb_publishable_q5EtMfCMg3aIhdNeDUKbig_4d_8YtAg',
+  businessTimeZone: 'Asia/Shanghai'
 };
